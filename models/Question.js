f3ec4d114a50376719cb74d6c0594eb0ef802410
PJ Frias <peejfrias@gmail.com>
2017-11-19 12:37:23 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -7,10 +7,10 @@ class Question{
         id INTEGER PRIMARY KEY,
         content TEXT
       )`
-      
+
       db.run(sql, function(){
         resolve("questions table created")
-      })      
+      })
     })
   }
 
@@ -19,31 +19,31 @@ class Question{
   }
 
   insert(){
-    const self = this 
+    const self = this
     const sql = `INSERT INTO questions (content) VALUES (?)`
     return new Promise(function(resolve){
       db.run(sql, [self.content], function(err, result){
         self.id = this.lastID
-        resolve(self)      
+        resolve(self)
       })
     })
   }
 
   static Find(id){
     const sql = `SELECT * FROM users WHERE id = ? LIMIT 1`
- 
+
     console.log(`Querying for user id ${id}...`)
- 
+
     return new Promise(function(resolve){
       db.get(sql, [id], function(err, resultRow){
         console.log(`...found ${JSON.stringify(resultRow)}!`)
- 
+
         const user = new User(resultRow.name, resultRow.age)
         user.id = resultRow.id
- 
-        resolve(user)        
+
+        resolve(user)
       })
-    })  
+    })
   }
 
 }
